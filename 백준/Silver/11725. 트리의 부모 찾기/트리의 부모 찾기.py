f73b4cf93fa329f5fcbe969ee38543507e182707
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-01-05 13:52:11 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 284 ms, Memory: 66216 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,10 +1,9 @@
-from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6) #재귀 깊이 때문에 recursionError 나서 추가
 input=sys.stdin.readline
 n=int(input())
 
-graph=[[] for i in range(n+1)]
+graph=[[] for i in range(n+1)] #n by n에서 줄임 (n의 값 최대가 100000 이기 때문)
 for i in range(1,n):
     a,b=map(int,input().split())
     graph[a].append(b)

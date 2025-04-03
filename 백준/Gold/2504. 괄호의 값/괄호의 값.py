ae97f071a4b798deed3746795bcd6b5dd1ff4579
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-04-03 22:30:57 +0900

    [Gold V] Title: 괄호의 값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,37 +1,38 @@
-string=input()
-stack=[]
-res=''
-flag=False
-for _ in string:
-    if _=='(':
-        stack.append("(")
-        res+=_
-    elif _=='[':
-        stack.append("[")
-        res+=_
-    elif _ ==')':
-        if len(stack) ==0 or stack[-1]!='(':
-            flag=True
-            break
-        else:
-            stack.pop()
-            res+=(")*2")
-    elif _ ==']':
-        if len(stack) ==0 or stack[-1]!='[':
-            flag=True
-            break
-        else:
-            stack.pop()
-            res+=("]*3")
-if flag or len(stack) !=0:
-    print(0)
-    exit()
-else:
-    try:
-        res=res.replace("()","(1)").replace("[]","(1)")
-        res=res.replace("[","(").replace("]",")")
-        res=res.replace("2(","2+(").replace("3(","3+(")
 
-        print(eval(res))
-    except:
-        print(0)
+arr=input()
+stack=[]
+
+answer=0
+tmp=1
+for i in range(len(arr)):
+    if arr[i] =='(':
+        stack.append(arr[i])
+        tmp *=2
+    elif arr[i] == '[':
+        stack.append(arr[i])
+        tmp *=3
+    elif arr[i] == ")":
+        if not stack or stack[-1] == "[":
+            answer = 0 # 실패
+            break
+        if arr[i-1] == "(":
+            answer += tmp
+        stack.pop()
+        tmp //= 2  #tmp 초기화
+    else:
+        if not stack or stack[-1] == "(":
+            answer=0
+            break
+        if arr[i-1] =='[':
+            answer+=tmp
+        stack.pop()
+        tmp //=3 #tmp 초기화
+
+if stack:
+    print(0)
+else:
+    print(answer)
+
+
+# 큰 괄호 안에 작은 괄호끼리 더해져 있을 때 이를 괄호 두개로 나눠서 생각 
+# 작은 괄호 때문에 곱해져 있는거만큼 다시 나눠서 뒤에 있는 작은 괄호에 영향 안주도록 나눔 

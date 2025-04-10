7c68846261bd7929617e6e22fff1615c72344b57
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-04-10 21:13:24 +0900

    [Gold V] Title: 동전 2, Time: 416 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -6,20 +6,18 @@ for i in range(n):
 coins=sorted(coins)
 #불가능한 경우는 -1 출력
 #최소한의 동전 이용하는게 목표
-dp=[0]*(k+1)
-
+dp=[10001]*(k+1)
+dp[0]=0
 for c in coins:
     for i in range(c,k+1,c):
         dp[i]=dp[i-c]+1
     #print(dp)
 
 for c in coins:
-    for i in range(c,k+1):
-        if dp[i]!=0 and dp[i-c]!=0:
-            dp[i]=min(dp[i],dp[i-c]+1)
-        elif dp[i]==0 and dp[i-c]!=0:
-            dp[i]=dp[i-c]+1
-if dp[k]==0:
+    for i in range(c, k+1):
+        dp[i]=min(dp[i],dp[i-c]+1)
+        
+if dp[k]==10001:
     print(-1)
 else:
     print(dp[k])

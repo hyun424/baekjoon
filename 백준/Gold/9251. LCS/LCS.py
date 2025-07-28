c379d2adea6ace7e6b042318598be75a09ad351f
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-07-28 18:51:52 +0900

    [Gold V] Title: LCS, Time: 120 ms, Memory: 118548 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -2,10 +2,7 @@ a=input()
 b=input()
 
 dp=[[0]*(len(b)+1) for _ in range(len(a)+1)]
-for i in range(len(a)):
-    for j in range(len(b)):
-        if a[i] == b[j]:
-            dp[i+1][j+1] = 1
+
 for i in range(len(a)):
     for j in range(len(b)):
         if a[i] == b[j]:

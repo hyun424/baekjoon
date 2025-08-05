a581a0df93544ab91d50edac793e886961cc2776
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-08-05 21:26:14 +0900

    [Gold V] Title: 토마토, Time: 1312 ms, Memory: 100476 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,40 +1,57 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+# 첫 줄에서 상자의 가로 크기 M과 세로 크기 N을 입력받습니다.
+M, N = map(int, sys.stdin.readline().split())
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+# 이후 N개의 줄에 걸쳐 토마토 상자의 상태 정보를 2차원 리스트로 입력받습니다.
+box = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-queue = deque()
-res = [[-1]*m for _ in range(n)]
+# 1)
+# 전체 탐색 후 0이 없으면 -> 0
+# 우선 익은 토마토(1) 큐에 넣어두기
+# 2)
+# 큐에서 하나씩 빼고 상하좌우에 0있으면 큐에넣고 1로 바꾸기
+# 큐 끝나고 다시 전체탐색 후 0 남아있으면 -> -1
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            queue.append((i, j))
-            res[i][j] = 0
-        elif graph[i][j] == -1:
-            res[i][j] = -1  # 벽(토마토 없음)
+def solution (box, M, N) :
+    queue = deque()
+    answer = 0
+    hasZero = False
 
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if graph[nx][ny] == 0 and res[nx][ny] == -1:
-                res[nx][ny] = res[x][y] + 1
-                queue.append((nx, ny))
+    for i in range(N) :
+        for j in range(M) :
+            if box[i][j] == 1 :
+                queue.append((i, j, 0))
+            elif box[i][j] == 0:
+                hasZero = True
+    
+    if hasZero == False :
+         return 0
+    
+    while queue:
+        x, y, day = queue.popleft()
 
-ans = 0
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0 and res[i][j] == -1:
-            print(-1)
-            exit()
-        if res[i][j] > ans:
-            ans = res[i][j]
-print(ans)
+        if day > answer :
+            answer = day
+
+        dx = [-1, 1, 0, 0]      # 상, 하
+        dy = [0, 0, -1, 1]      # 좌, 우
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+        
+            if 0 <= nx < N and 0 <= ny < M and box[nx][ny] == 0:
+                queue.append((nx, ny, day+1))
+                box[nx][ny] = 1
+        
+    for i in range(N) :
+        for j in range(M) :
+            if box[i][j] == 0 :
+                return -1
+
+    return answer
+
+
+print(solution(box, M, N))

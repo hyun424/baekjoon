757edae45d8d0e09af5246e47c8a9670c83a33fc
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-08-11 16:29:41 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -3,21 +3,21 @@ input = sys.stdin.readline
 
 n = int(input())
 stack = []
-output = []
+out = []
 
 for _ in range(n):
     cmd = input().split()
+    op = cmd[0]
 
-    if cmd[0] == "push":
-        stack.append(cmd[1])
-    elif cmd[0] == "pop":
-        output.append(stack.pop() if stack else "-1")
-    elif cmd[0] == "size":
-        output.append(str(len(stack)))
-    elif cmd[0] == "empty":
-        output.append("0" if stack else "1")
-    elif cmd[0] == "top":
-        output.append(stack[-1] if stack else "-1")
+    if op == "push":
+        stack.append(cmd[1])                 # int() 변환하지 않음
+    elif op == "pop":
+        out.append(stack.pop() if stack else "-1")
+    elif op == "size":
+        out.append(str(len(stack)))
+    elif op == "empty":
+        out.append("0" if stack else "1")
+    elif op == "top":
+        out.append(stack[-1] if stack else "-1")
 
-# 한 번에 출력
-sys.stdout.write("\n".join(output))
+sys.stdout.write("\n".join(out))

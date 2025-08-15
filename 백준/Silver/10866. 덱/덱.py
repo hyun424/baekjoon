126b3e4859b0460feb783bd1310629e40bac5a16
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-08-15 10:34:27 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+n = int(input())
+queue=deque()
+for _ in range(n):
+    cmd=list(input().split())
+    if cmd[0] == "push_back":
+        queue.append(cmd[1])
+    elif cmd[0] == "push_front":
+        queue.appendleft(cmd[1])
+    
+    elif cmd[0] == "pop_front":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    
+    elif cmd[0] == "pop_back":
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
+    
+    elif cmd[0] == "size":
+        print(len(queue))
+    
+    elif cmd[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+
+    elif cmd[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    
+    elif cmd[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)

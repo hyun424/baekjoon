5f59ce7c15685a2880deb36899b01f3ef9da298d
hyun424 <56660134+hyun424@users.noreply.github.com>
2025-08-27 15:51:41 +0900

    [Gold V] Title: 토마토, Time: 652 ms, Memory: 224284 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,53 +1,43 @@
 import sys
 from collections import deque
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-m,n,h=map(int,input().split())
-box=[[[0]*(m) for _ in range(n)] for i in range(h)]
-# box[h][n][m]
-#전후좌우상하
-dx=[0,0,-1,1,0,0]
-dy=[-1,1,0,0,0,0]
-dz=[0,0,0,0,-1,1]
-for i in range(h):
-    for j in range(n):
-        box[i][j]=list(map(int,input().split()))
+M, N, H = map(int, input().split())
+box = [[[0]*M for _ in range(N)] for _ in range(H)]
 
+for h in range(H):
+    for n in range(N):
+        box[h][n] = list(map(int, input().split()))
 
-queue=deque()
+q = deque()
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if box[h][n][m] == 1:
+                q.append((h, n, m))
 
+# 상하좌우전후 (z,y,x)
+dz = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dx = [0, 0, 0, 0, -1, 1]
 
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if box[i][j][k]==1:
-                queue.append((i,j,k))
-visited=[[[False]*m for _ in range(n)] for i in range(h)]
+while q:
+    z, y, x = q.popleft()
+    for d in range(6):
+        nz, ny, nx = z + dz[d], y + dy[d], x + dx[d]
+        if 0 <= nz < H and 0 <= ny < N and 0 <= nx < M:
+            if box[nz][ny][nx] == 0:                # 아직 안 익음이면
+                box[nz][ny][nx] = box[z][y][x] + 1  # 일수 기록(전염)
+                q.append((nz, ny, nx))
 
-while queue:
-    x,y,z= queue.popleft()
-    visited[x][y][z]=True
-    for i in range(6):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        nz=z+dz[i]
-        if 0<=nx<h and 0<=ny<n and 0<=nz<m:
-            if box[nx][ny][nz]==0 and not visited[nx][ny][nz]:
-                visited[nx][ny][nz]=True
-                box[nx][ny][nz]=box[x][y][z]+1
-                queue.append((nx,ny,nz))
-max=1
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if box[i][j][k]==0:
+max_day = 1
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if box[h][n][m] == 0:
                 print(-1)
-                exit()
-            else:
-                if max<box[i][j][k]:
-                    max=box[i][j][k]
-if max==1:
-    print(0)
+                sys.exit(0)
+            if box[h][n][m] > max_day:
+                max_day = box[h][n][m]
 
-else:
-    print(max-1)
+print(0 if max_day == 1 else max_day - 1)

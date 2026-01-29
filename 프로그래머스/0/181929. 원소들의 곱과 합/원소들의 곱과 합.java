1c9dc7672dca7d5c52e4c20d69d3c8ac01e09e83
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-01-30 01:53:26 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int multiple = 1;
+        int sum = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            multiple *= num_list[i];
+            sum += num_list[i];
+        }
+
+        int sum_square = sum * sum;
+
+        return (multiple < sum_square) ? 1 : 0;
+    }
+}

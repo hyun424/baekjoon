7ee9e4d8b8d7d62c191c76bf3225171934fbf1de
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-02-11 20:34:02 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        int len = str1.length();
+        for (int i = 0; i < len; i++){
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
+        }
+        return sb.toString();
+    }
+}

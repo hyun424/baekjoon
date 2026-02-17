21c5ca8d82da3bb00a136bd9fa07d7da6c4c47e1
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-02-17 18:30:07 +0900

    [level 1] Title: 기사단원의 무기, Time: 8.28 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+
+        int[] divisor = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = i; j <= number; j += i) {
+                divisor[j]++;
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (divisor[i] > limit) {
+                answer += power;
+            } else {
+                answer += divisor[i];
+            }
+        }
+
+        return answer;
+    }
+}

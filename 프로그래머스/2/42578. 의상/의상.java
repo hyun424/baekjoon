628d7e38751ce615a895b9ee6f5804bd0e0c7170
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-02-18 15:35:21 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,24 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[][] clothes) {
-
-        Map<String, Integer> map = new HashMap<>();
-
-        for (String[] c : clothes) {
-            String category = c[1];
-
-            map.put(category, map.getOrDefault(category, 0) + 1);
-        }
-
-        int answer = 1;
-
-        // 조합 계산
-        for (int count : map.values()) {
-            answer *= (count + 1);
-        }
-
-        // 아무것도 안 입는 경우 제외
-        return answer - 1;
+        int answer = 0;
+        return answer;
     }
 }

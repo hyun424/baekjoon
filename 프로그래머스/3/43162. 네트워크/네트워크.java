6c78958935fb159f65831e754b45a4bcfe53cbab
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-02-19 17:32:58 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i, computers, visited);
+            }
+        }
+        return answer;
+    }
+
+    private void dfs(int cur, int[][] computers, boolean[] visited) {
+        visited[cur] = true;
+
+        for (int next = 0; next < computers.length; next++) {
+            if (computers[cur][next] == 1 && !visited[next]) {
+                dfs(next, computers, visited);
+            }
+        }
+    }
+}

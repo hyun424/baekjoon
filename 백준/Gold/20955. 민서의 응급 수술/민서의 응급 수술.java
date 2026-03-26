8423506fa4523dac1d539b5ced099d9ec5760f1a
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-03-26 16:54:10 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 328 ms, Memory: 42244 KB -BaekjoonHub

---
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int removeCount = 0;
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            if (find(u) == find(v)) {
+                removeCount++;
+            } else {
+                union(u, v);
+            }
+        }
+
+        int componentCount = 0;
+        for (int i = 1; i <= n; i++) {
+            if (find(i) == i) {
+                componentCount++;
+            }
+        }
+
+        System.out.println(removeCount + componentCount - 1);
+    }
+}

adce7a293f41e01291d117df6dda0c6f1d61a201
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-04-13 21:44:36 +0900

    [Gold IV] Title: 팰린드롬?, Time: 696 ms, Memory: 218620 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -3,22 +3,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] num;
-    public static int isPalindrome(int x, int y) {
-        for(int i = 0; i <= y - x; i++){
-            if (num[x - 1 + i] != num[y - 1 - i]) return 0;
-        }
-        return 1;
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        num = new int[n];
+        int[] num = new int[n + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
+
+        boolean[][] dp = new boolean[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (num[i] == num[i + 1]) {
+                dp[i][i + 1] = true;
+            }
+        }
+
+        for (int len = 3; len <= n; len++) {
+            for (int start = 1; start + len - 1 <= n; start++) {
+                int end = start + len - 1;
+                if (num[start] == num[end] && dp[start + 1][end - 1]) {
+                    dp[start][end] = true;
+                }
+            }
+        }
+
         int m = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
@@ -26,9 +41,8 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            sb.append(isPalindrome(x, y)).append('\n');
+            sb.append(dp[x][y] ? 1 : 0).append('\n');
         }
         System.out.println(sb);
-        // int n = Integer.parseInt(br.readLine());
     }
 }

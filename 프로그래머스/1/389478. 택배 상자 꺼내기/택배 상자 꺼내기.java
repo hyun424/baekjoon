24afec1feb6c958b941c5a73dc3fb3c5560f8942
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-04-26 16:05:45 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,40 +1,24 @@
 class Solution {
     public int solution(int n, int w, int num) {
-
-        int row = (num - 1) / w;
+        int floor = (num - 1) / w;
         int pos = (num - 1) % w;
-
+        
         int col;
-        if (row % 2 == 0) {
-            col = pos;
-        } else {
-            col = w - 1 - pos;
+        if (floor % 2 == 0) col = pos;
+        else col = w - 1 - pos;
+        
+        int totalFloor = (n - 1) / w;
+        int answer = 0;
+        
+        for (int r = floor; r <= totalFloor; r++) {
+            int box;
+            
+            if (r % 2 == 0) box = r * w + col + 1;
+            else box = r * w + (w - col);
+            
+            if (box <= n) answer++;
         }
-
-        int totalRows = (n - 1) / w;
-        int answer = 1;
-
-        for (int r = row + 1; r <= totalRows; r++) {
-
-            int start = r * w + 1;
-            int end = Math.min(n, (r + 1) * w);
-
-            int count = end - start + 1;
-
-            if (count <= 0) continue;
-
-            int checkCol;
-            if (r % 2 == 0) {
-                checkCol = col;
-            } else {
-                checkCol = w - 1 - col;
-            }
-
-            if (checkCol < count) {
-                answer++;
-            }
-        }
-
+        
         return answer;
     }
 }

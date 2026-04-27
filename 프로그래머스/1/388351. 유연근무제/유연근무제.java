7ca8ce48ca1b7766f7d5c981d61790562e266a00
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-04-27 12:57:07 +0900

    [level 1] Title: 유연근무제, Time: 0.51 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,32 +1,42 @@
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
         int answer = 0;
+        int n = schedules.length;
 
-        for (int i = 0; i < schedules.length; i++) {
-            int hope = schedules[i];
-            int hopeMin = (hope / 100) * 60 + (hope % 100);
-            int limit = hopeMin + 10;
-
-            boolean ok = true;
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            int day = startday;
+            int limit = addTenMinutes(schedules[i]);
 
             for (int j = 0; j < 7; j++) {
-                int day = (startday + j - 1) % 7 + 1; // 1~7
-
-                // 토(6), 일(7)은 무시
-                if (day == 6 || day == 7) continue;
-
-                int real = timelogs[i][j];
-                int realMin = (real / 100) * 60 + (real % 100);
-
-                if (realMin > limit) {
-                    ok = false;
-                    break;
+                // 평일만 체크
+                if (day != 6 && day != 7) {
+                    if (timelogs[i][j] > limit) {
+                        flag = false;
+                        break;
+                    }
                 }
+
+                day++;
+                if (day == 8) day = 1;
             }
 
-            if (ok) answer++;
+            if (flag) answer++;
         }
 
         return answer;
+    }
+
+    private int addTenMinutes(int time) {
+        int hour = time / 100;
+        int minute = time % 100;
+
+        minute += 10;
+        if (minute >= 60) {
+            hour++;
+            minute -= 60;
+        }
+
+        return hour * 100 + minute;
     }
 }

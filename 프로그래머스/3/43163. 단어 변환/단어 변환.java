11834349fb583a459fbe9e72cc740857e29a8c5d
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-07-23 18:50:54 +0900

    [level 3] Title: 단어 변환, Time: 0.52 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,49 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        // target이 words에 없으면 불가능
-        boolean exists = false;
-        for (String w : words) {
-            if (w.equals(target)) { exists = true; break; }
+    boolean[] visited;
+    class tempword{
+        String word;
+        int res;
+        tempword(String word, int res){
+            this.word = word;
+            this.res = res;
         }
-        if (!exists) return 0;
-
-        Queue<String> q = new ArrayDeque<>();
-        Queue<Integer> depth = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
-
-        q.add(begin);
-        depth.add(0);
-
-        while (!q.isEmpty()) {
-            String cur = q.poll();
-            int d = depth.poll();
-
-            if (cur.equals(target)) return d;
-
-            for (int i = 0; i < words.length; i++) {
-                if (visited[i]) continue;
-                if (diffOne(cur, words[i])) {
+    }
+    public static boolean canChange(String A, String B){
+        int temp = 0;
+        for(int i = 0; i < A.length(); i++){
+            if(A.charAt(i) != B.charAt(i)) temp++;
+        }
+        if (temp == 1) return true;
+        return false;
+    }
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        int size = words.length;
+        visited = new boolean[size];
+        Queue<tempword> q = new ArrayDeque <>();
+        
+        q.offer(new tempword(begin, 0));
+        
+        while(!q.isEmpty()){
+            
+            tempword cur = q.poll(); 
+            
+            String word = cur.word;
+            int res = cur.res;
+            
+            if (word.equals(target)) return res;
+            
+            for(int i = 0; i < size; i++){
+                
+                if(!visited[i] && canChange(word, words[i])){
+                    
                     visited[i] = true;
-                    q.add(words[i]);
-                    depth.add(d + 1);
+                    q.offer(new tempword(words[i], res + 1));
                 }
             }
         }
-
-        return 0;
-    }
-
-    private boolean diffOne(String a, String b) {
-        int diff = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) diff++;
-            if (diff > 1) return false;
-        }
-        return diff == 1;
+        return answer;
     }
 }

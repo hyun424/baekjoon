441c9a1cfb01f26664834fae175eae0e643c2781
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-07-23 19:45:42 +0900

    [level 2] Title: 피로도, Time: 1.54 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,34 @@
 class Solution {
 
-    static boolean[] visited;
-    static int max = 0;
+    private boolean[] visited;
+    private int max;
 
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
+        max = 0;
+
         dfs(k, dungeons, 0);
+
         return max;
     }
 
     private void dfs(int fatigue, int[][] dungeons, int count) {
-
         max = Math.max(max, count);
 
         for (int i = 0; i < dungeons.length; i++) {
-
-            // 아직 방문 안했고, 최소 필요 피로도 충족하면
+            // 아직 방문하지 않았고,
+            // 현재 피로도가 최소 필요 피로도 이상인 경우
             if (!visited[i] && fatigue >= dungeons[i][0]) {
-
                 visited[i] = true;
-                dfs(fatigue - dungeons[i][1], dungeons, count + 1);
-                visited[i] = false; // 백트래킹
+
+                dfs(
+                    fatigue - dungeons[i][1],
+                    dungeons,
+                    count + 1
+                );
+
+                // 다른 순서도 탐색해야 하므로 방문 복구
+                visited[i] = false;
             }
         }
     }

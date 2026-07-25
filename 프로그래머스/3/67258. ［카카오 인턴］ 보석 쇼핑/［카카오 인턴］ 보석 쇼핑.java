2bab1727650c162dcaea1386b6263a2470545d43
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-07-25 20:31:33 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 33.77 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,37 +2,43 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        Set<String> set = new HashSet<>(Arrays.asList(gems));
-        int totalType = set.size();
+        int kindCount = new HashSet<>(Arrays.asList(gems)).size();
 
         Map<String, Integer> map = new HashMap<>();
 
-        int left = 0;
-        int right = 0;
+        int start = 0;
+        int bestStart = 0;
+        int bestEnd = gems.length - 1;
 
-        int bestLeft = 0;
-        int bestRight = gems.length - 1;
-        int minLen = Integer.MAX_VALUE;
+        for (int end = 0; end < gems.length; end++) {
+            // 오른쪽 보석 추가
+            map.put(
+                gems[end],
+                map.getOrDefault(gems[end], 0) + 1
+            );
 
-        while (right < gems.length) {
-            map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
-            right++;
-
-            while (map.size() == totalType) {
-                if (right - left < minLen) {
-                    minLen = right - left;
-                    bestLeft = left;
-                    bestRight = right - 1;
+            // 모든 종류가 들어 있다면 왼쪽을 최대한 줄임
+            while (map.size() == kindCount) {
+                // 지금까지 찾은 구간보다 짧으면 갱신
+                if (end - start < bestEnd - bestStart) {
+                    bestStart = start;
+                    bestEnd = end;
                 }
 
-                map.put(gems[left], map.get(gems[left]) - 1);
-                if (map.get(gems[left]) == 0) {
-                    map.remove(gems[left]);
+                // 왼쪽 보석 제거
+                String startGem = gems[start];
+                map.put(startGem, map.get(startGem) - 1);
+
+                // 개수가 0이면 현재 구간에 없는 종류이므로 삭제
+                if (map.get(startGem) == 0) {
+                    map.remove(startGem);
                 }
-                left++;
+
+                start++;
             }
         }
 
-        return new int[]{bestLeft + 1, bestRight + 1};
+        // 배열 인덱스는 0부터, 진열대 번호는 1부터
+        return new int[]{bestStart + 1, bestEnd + 1};
     }
 }

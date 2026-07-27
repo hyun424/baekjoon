82665c350d4b93b3e65da19f35d35c278dd4a498
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-07-27 11:14:05 +0900

    [level 2] Title: 타겟 넘버, Time: 3.35 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,39 +1,23 @@
-import java.util.*;
-
 class Solution {
-
-    static class State {
-        int idx;
-        int sum;
-
-        State(int idx, int sum) {
-            this.idx = idx;
-            this.sum = sum;
-        }
-    }
-
+    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-
-        Queue<State> q = new ArrayDeque<>();
-        q.offer(new State(0, 0));
-
-        while (!q.isEmpty()) {
-            State cur = q.poll();
-
-            if (cur.idx == numbers.length) {
-                if (cur.sum == target) {
-                    answer++;
-                }
-                continue;
-            }
-
-            int num = numbers[cur.idx];
-
-            q.offer(new State(cur.idx + 1, cur.sum + num));
-            q.offer(new State(cur.idx + 1, cur.sum - num));
-        }
-
+        
+        int sum = 0;
+        dfs(0,0, numbers, target);
+        
         return answer;
+    }
+    
+    
+    private static void dfs(int sum, int index, int[] numbers,int target){
+        if(index == numbers.length){
+            if(sum == target){
+                answer++;
+            }
+            return;
+        }
+        dfs(sum + numbers[index], index + 1, numbers, target);
+        dfs(sum - numbers[index], index + 1, numbers, target);
+        
     }
 }

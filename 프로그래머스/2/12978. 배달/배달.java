bf5f46846bdd93be5b90a20c5d4d2b629f38d1b2
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-07-27 17:07:23 +0900

    [level 2] Title: 배달, Time: 2.40 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,23 +2,27 @@ import java.util.*;
 
 class Solution {
     
+
+    
     private class Node{
         int to;
         int cost;
-        
         Node(int to, int cost){
             this.to = to;
             this.cost = cost;
         }
     }
     
-    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
+        int INF = Integer.MAX_VALUE / 4;
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[1] = 0;
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
         
-        List<List<Node>> graph = new ArrayList<>();
         for(int i = 0; i <= N; i++){
-            graph.add(new ArrayList<>());
+            graph.add(new ArrayList());
         }
         
         for(int[] r : road){
@@ -28,31 +32,21 @@ class Solution {
             graph.get(a).add(new Node(b, cost));
             graph.get(b).add(new Node(a, cost));
         }
-
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
-        PriorityQueue<Node> pq = 
-            new PriorityQueue<>(Comparator.comparingInt(node -> node.cost));
+        PriorityQueue <Node> pq = new PriorityQueue<>((a,b) -> (a.cost - b.cost));
         pq.offer(new Node(1,0));
-        
         while(!pq.isEmpty()){
-            Node current = pq.poll();
-            if(current.cost > dist[current.to]){
-                continue;
-            }
-            
-            for(Node next : graph.get(current.to)){
-                int newCost = current.cost + next.cost;
-                
-                if (newCost < dist[next.to]){
-                    dist[next.to] = newCost;
-                    pq.offer(new Node(next.to, newCost));
+            Node cur = pq.poll();
+            for(Node n : graph.get(cur.to)){
+                if(cur.cost > dist[cur.to]) continue;
+                int newCost = cur.cost + n.cost;
+                if(newCost < dist[n.to]){
+                    dist[n.to] = newCost;
+                    pq.offer(new Node(n.to, newCost));
                 }
             }
         }
-      
-        for (int i = 0; i <= N; i++){
+        
+        for(int i = 1; i <= N; i++){
             if (dist[i] <= K){
                 answer++;
             }
@@ -60,3 +54,6 @@ class Solution {
         return answer;
     }
 }
+/*
+
+*/

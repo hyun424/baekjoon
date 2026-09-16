327b22d26abff17c54a7d9cc890a4d7c4eb38d15
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-09-16 20:35:56 +0900

    [level 2] Title: 요격 시스템, Time: 293.91 ms, Memory: 172 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] targets) {
+        Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int answer = 0;
+        int lastEnd = -1;
+
+        for (int[] target : targets) {
+            if (target[0] >= lastEnd) {
+                answer++;
+                lastEnd = target[1];
+            }
+        }
+
+        return answer;
+    }
+}

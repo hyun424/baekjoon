f55e61227723316d26cd86f28629977cb698da94
hyun424 <56660134+hyun424@users.noreply.github.com>
2026-09-19 23:53:05 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 4.43 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939#) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 8.21 ms
+메모리: 87.2 MB, 시간: 4.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 19일 22:25:45
+2026년 09월 19일 23:53:02
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,21 +1,16 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
         String[] words = s.split(" ");
-
-        int[] nums = Arrays.stream(words)
-                           .mapToInt(str -> Integer.parseInt(str))
-                           .toArray();
-
-        int min = Arrays.stream(nums)
-                        .min()
-                        .getAsInt();
-
-        int max = Arrays.stream(nums)
-                        .max()
-                        .getAsInt();
-
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(String word : words){
+            int number = Integer.parseInt(word);
+            
+            min = Math.min(min, number);
+            max = Math.max(max, number);
+            
+        }
         return min + " " + max;
     }
 }
